fbfe18fb27470a4b33711fe9e0a119100bce1b19
Krish2324 <krack232004@gmail.com>
2023-09-05 16:47:01 +0530

    New calculator prac

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Pracalculator from '../src/components/pracalculator';
+import './App.css';
+
+function App() {
+    return ( 
+        <div className = "PracticeContainer" >
+        <Pracalculator / >
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/pracalculator.js
@@ -0,0 +1,123 @@
+import React from 'react'
+import { Button } from "react-bootstrap";
+import { FaBackspace } from "react-icons/fa";
+import { FaDivide } from "react-icons/fa";
+
+
+
+function Pracalculator() {
+
+    const ChangeStyle=() =>{
+        const targetElement = document.getElementById("target-element");
+        console.log(targetElement,"Debugg");
+        // Check the current CSS class and toggle it to switch styles
+        if (targetElement.classList.contains("Calculator-Heading")) {
+            console.log("Checked Here")
+            targetElement.classList.remove("Calculator-Heading");
+            targetElement.classList.add("alternate-style");
+        } else {
+            console.log("Checked")
+
+            targetElement.classList.remove("alternate-style");
+            targetElement.classList.add("Calculator-Heading");
+        }
+    }
+
+    return ( 
+        <div className = 'Calculator'>
+            <div className = 'Calculator-Wrapper'>
+            <div className = 'Calculator-Heading Aligned'  id="target-element">
+                <h3 className = "Disable">Calculator In React </h3>
+            </div>
+
+            <div className='Calculator-Screen Aligned'>
+                <div className='Calculator-Output'>
+                    <span>
+
+                    </span>
+                </div>
+            </div>
+
+                <div className = 'Calculator-Input Aligned'>
+                    <span>
+
+                    </span>
+                </div>
+
+                <div className='Calculator-Button'>
+                {/* First line */}
+                    <Button type='button' className='Top-Btn' value="AC">
+                        AC
+                    </Button>
+                    <Button type='button' className='Top-Btn' value="PM">
+                        +/-
+                    </Button>
+                    <Button type='button' className='Top-Btn' value="%">
+                        %
+                    </Button>
+                    <Button type='button' className='Special-Btn' value="">
+                        <FaBackspace/>
+                    </Button>
+             {/* Second line */}
+                    <Button type='button' className='Normal-Btn' value="7">
+                        7
+                    </Button>
+                    <Button type='button' className='Normal-Btn' value="8">
+                        8
+                    </Button>
+                    <Button type='button' className='Normal-Btn' value="9">
+                        9
+                    </Button>
+                    <Button type='button' className='Special-Btn' value="x">
+                        x
+                    </Button>
+                {/* Third Line */}
+                    <Button type='button' className='Normal-Btn' value="4">
+                        4
+                    </Button>
+                    <Button type='button' className='Normal-Btn' value="5">
+                        5
+                    </Button>
+                    <Button type='button' className='Normal-Btn' value="6">
+                        6
+                    </Button>
+                    <Button type='button' className='Special-Btn' value="">
+                        <FaDivide/>
+                    </Button>
+                {/* Fourth Line */}
+                    <Button type='button' className='Normal-Btn' value="1">
+                        1
+                    </Button>
+                    <Button type='button' className='Normal-Btn' value="2">
+                        2
+                    </Button>
+                    <Button type='button' className='Normal-Btn' value="3">
+                        3
+                    </Button>
+                    <Button type='button' className='Special-Btn' value="-">
+                        _    
+                    </Button>
+                {/* Fifth line */}
+                    <Button type='button' className='Special-Btn' value=".">
+                        .
+                    </Button>
+                    <Button type='button' className='Normal-Btn' value="0">
+                        0
+                    </Button>
+                    <Button type='button' className='Special-Btn' value="+">
+                        +
+                    </Button>
+                    <Button type='button' className='Special-Btn' value="=">
+                        =
+                    </Button>
+                </div>
+        </div>
+    <button className="Calculator-Button" onClick={ChangeStyle}>Change Style</button>
+        
+        </div>
+        
+    )
+}
+
+export default Pracalculator;
+
